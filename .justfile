8c8f8272a22cc78977dc181a9f1373c99dcc85d4
Dominic Grimaldi <dev@grimoutlook.dev>
2026-02-24 20:27:46 -0600

    chore: init

---
+++ .justfile
@@ -0,0 +1,22 @@
+hostname := "belfast"
+export NH_FLAKE := "github:GrimOutlook/nix-host-{{hostname}}"
+
+default:
+  just --list
+
+us: update home
+
+alias switch := home
+
+home:
+  nh home switch . -c {{hostname}}
+
+os:
+  nh os switch . -H {{hostname}}
+
+update:
+  nix flake update
+
+check:
+  nix flake check
+
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "NixOS configuration for belfast";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+
+    nix-config = {
+      url = "github:GrimOutlook/nix-config";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ self, nix-config, ... }:
+    nix-config.inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        nix-config.modules.flake.hosts
+        nix-config.modules.flake.host-info
+        (nix-config + "/modules/flake/systems.nix")
+      ];
+      host-info.flake = "github:GrimOutlook/nix-host-belfast";
+      host = {
+        name = "belfast";
+        nixos = {
+          modules = with nix-config.modules.nixos; [
+            dev
+            wsl
+          ];
+          system = {
+            autoUpgrade.enable = true;
+            stateVersion = "25.05";
+          };
+        };
+        home = {
+          modules = with nix-config.modules.homeManager; [
+            dev
+            networking
+          ];
+          home.stateVersion = "25.11";
+        };
+      };
+    };
+}
